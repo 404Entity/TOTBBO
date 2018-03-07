65f4779793d3e34c1d6984544b49eb1fedea7333
Morten Ellegaard Hansen <mort140w@student.eadania.dk>
2018-03-07 20:23:51 +0100

    egre

---
+++ T_L_O_B_O/Component/ChestOfAThousandGrogs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_L_O_B_O
+{
+    class ChestOfAThousandGrogs : Component, ICollisionStay
+    {
+        //Fields
+
+
+
+
+
+        //Propertys
+
+        //Constructor
+        public ChestOfAThousandGrogs(GameObject gameObject): base(gameObject)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        //Methods
+        public void OnCollisionStay(Collider other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
