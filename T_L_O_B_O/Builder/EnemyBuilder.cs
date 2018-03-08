7d260909607068d2f1d3b9c83057fe5de834d75f
MIKZU <mikkel.rr@hotmail.com>
2018-03-08 10:36:55 +0100

    fixed enemy

--- T_L_O_B_O/Builder/EnemyBuilder.cs
+++ T_L_O_B_O/Builder/EnemyBuilder.cs
@@ -18,7 +18,7 @@ namespace T_L_O_B_O
             slime.AddComponent(new Animator(slime));
             slime.AddComponent(new Enemy(slime));
             slime.LoadContent(GameWorld.Instance.Content);
-            slime.AddComponent(new Collider(slime, false, 2));
+            slime.AddComponent(new Collider(slime, false, 0.5f));
             buildobject = slime;
         }
 
